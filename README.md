346afc9811397de42a05d98ff8ae7d5ec6672f6d
Itzik Lugassy <itziklugassy@h-MBP-sl-Itzik.lan>
2023-12-15 10:50:58 +0200

    add delte option and save

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Hello to my progrem 
+****Hello to my progrem**** 
+option progrem - car grage
+'a - add your car'
+'d - delete your car'
+'p - print all cars'
+'s - save cars to JSON file'
+'x - exit'
--- app.py
+++ app.py
@@ -1,18 +1,35 @@
 from icecream import ic
+import json
 
-cars=[]
+cars = []
 
 def main():
-    while(True):
-        ic("a- add your car")
-        ic("p- print all cars")
-        ic("x- exit")
-        user_selection = input("your selection")
+    while True:
+        ic("a - add your car")
+        ic("d - delete your car")
+        ic("p - print all cars")
+        ic("s - save cars to JSON file")
+        ic("x - exit")
+        user_selection = input("Your selection: ")
             
-        if user_selection =="a":cars.append('red')
-        if user_selection =="x":exit()
-        if user_selection =="p":ic(cars)
+        if user_selection == "a":
+            car_brand = input("Enter car brand: ")
+            cars.append(car_brand)
+        elif user_selection == "d":
+            car_to_delete = input("Enter car brand to delete: ")
+            if car_to_delete in cars:
+                cars.remove(car_to_delete)
+                ic(f"Car {car_to_delete} deleted.")
+            else:
+                ic(f"Car {car_to_delete} not found in the list.")
+        elif user_selection == "p":
+            ic("Cars:", cars)
+        elif user_selection == "s":
+            with open('cars.json', 'w') as file:
+                json.dump(cars, file)
+            ic("Cars saved to cars.json")
+        elif user_selection == "x":
+            break
 
-
-if __name__== '__main__':
+if __name__ == '__main__':
     main()
